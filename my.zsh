b32f556e932bda9862b4e40cfb24a65ab98b51aa
Matt Grimm <matt.t.grimm@gmail.com>
2019-03-01 11:05:16 -0700

    oh-my-zsh config

---
+++ my.zsh
@@ -0,0 +1,21 @@
+ZSH_THEME="norm"
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[blue]%}git %{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[yellow]%} → %{$reset_color%}"
+
+PROMPT='%{$fg[yellow]%}λ %{$fg[green]%}%c %{$fg[yellow]%}→ $(git_prompt_info)%{$reset_color%}'
+RPROMPT='%{$fg[yellow]%}$(virtualenv_prompt_info)%{$reset_color%}%'
+
+alias vi='nvim'
+alias vim='nvim'
+
+# lein
+alias repl='lein repl'
+alias lnr='lein run'
+alias lnc='lein clean'
+alias lni='lein install'
+alias lnt='lein test'
+
+# docker
+alias dp='docker ps'
+alias dk='docker kill'
