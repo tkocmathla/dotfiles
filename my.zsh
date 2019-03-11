8b6ab421aed951a5537780b71cfd8f9158516d28
Matt Grimm <matt.t.grimm@gmail.com>
2019-03-11 11:10:53 -0600

    Start shifting from lein to clj/deps

--- my.zsh
+++ my.zsh
@@ -6,11 +6,15 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[yellow]%} → %{$reset_color%}"
 PROMPT='%{$fg[yellow]%}λ %{$fg[green]%}%c %{$fg[yellow]%}→ $(git_prompt_info)%{$reset_color%}'
 RPROMPT='%{$fg[yellow]%}$(virtualenv_prompt_info)%{$reset_color%}%'
 
+# neovim
 alias vi='nvim'
 alias vim='nvim'
 
+# clojure
+alias repl='clj -A:cider'
+
 # lein
-alias repl='lein repl'
+#alias repl='lein repl'
 alias lnr='lein run'
 alias lnc='lein clean'
 alias lni='lein install'
