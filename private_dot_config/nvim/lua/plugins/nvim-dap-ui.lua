78c09f0a9c554430d21f3959260a5d6eca2c34de
tkocmathla <matt.t.grimm@gmail.com>
2024-09-11 18:07:52 -0600

    no want dap-ui without dap

--- private_dot_config/nvim/lua/plugins/nvim-dap-ui.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/rcarriga/nvim-dap-ui?tab=readme-ov-file#configuration
-return {
-    "rcarriga/nvim-dap-ui",
-    config = function()
-        require("dapui").setup()
-    end
-}
