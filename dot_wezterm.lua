cc36aad6c339fa0048a23a866e4b36724d7e4ee9
tkocmathla <matt.t.grimm@gmail.com>
2024-09-12 16:47:57 -0600

    map laptop pgdn/pgup keys in wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,6 +5,18 @@ config.color_scheme = 'Catppuccin Frappe'
 
 config.keys = {
   {
+    -- System76 LemurPro laptop "PgUp" key
+    key = 'raw:104',
+    mods = 'NONE',
+    action = wezterm.action.SendKey { key = 'PageUp' },
+  },
+  {
+    -- System76 LemurPro laptop "PgDn" key
+    key = 'raw:109',
+    mods = 'NONE',
+    action = wezterm.action.SendKey { key = 'PageDown' },
+  },
+  {
     key = 'f',
     mods = 'SHIFT|CTRL',
     action = wezterm.action.ToggleFullScreen,
