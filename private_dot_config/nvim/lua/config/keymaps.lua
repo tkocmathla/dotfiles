8992f487fb8c9a3d3f2ef6b63d407da86cef510b
tkocmathla <matt.t.grimm@gmail.com>
2024-09-24 10:27:06 -0600

    more zsh aliases; force git-status keymap in nvim

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -34,7 +34,7 @@ wk.register({
 
 -- Git
 vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<cr>")
-vim.keymap.set("n", "<leader>gs", ":G<cr>", { desc = "Git status" })
+vim.keymap.set("n", "<leader>gs", ":G<cr>", { desc = "Git status" }, { remap = true })
 vim.keymap.set("n", "<leader>gd", ":Gdiff<cr>", { desc = "Git diff" })
 vim.keymap.set("n", "<leader>gm", ":Gvdiffsplit!<cr>", { desc = "Git 3-way merge" })
 vim.keymap.set("n", "<leader>gM", ":G mergetool<cr>", { desc = "Git merge-tool" })
