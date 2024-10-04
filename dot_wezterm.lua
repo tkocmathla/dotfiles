05a40847fe21a4b30bc0b4d62023c0b52e61a16e
tkocmathla <matt.t.grimm@gmail.com>
2024-10-04 16:22:46 -0600

    add wezterm keybind to edit config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -10,6 +10,7 @@ config.keys = {
   { key = 'r', mods = 'LEADER', action = act.ReloadConfiguration },
   { key = 'a', mods = 'LEADER', action = act.PaneSelect },
   { key = 'o', mods = 'LEADER', action = act.RotatePanes 'Clockwise' },
+  { key = 'c', mods = 'LEADER', action = act.SpawnCommandInNewTab { args = { 'nvim', wezterm.home_dir .. '/.wezterm.lua' }}},
 }
 
 config.hide_tab_bar_if_only_one_tab = true
