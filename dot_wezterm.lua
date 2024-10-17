4a14d2486fff65e1e60b449c9499cd7af5682696
tkocmathla <matt.t.grimm@gmail.com>
2024-10-17 14:06:09 -0600

    various updates

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -6,14 +6,22 @@ config.color_scheme = 'Catppuccin Frappe'
 
 config.leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
+  { key = 'c', mods = 'LEADER', action = act.SpawnCommandInNewTab { args = { 'nvim', wezterm.home_dir .. '/.wezterm.lua' }}},
+
   { key = 'f', mods = 'LEADER', action = act.ToggleFullScreen },
   { key = 'r', mods = 'LEADER', action = act.ReloadConfiguration },
+
+  -- Pane selection
   { key = 'a', mods = 'LEADER', action = act.PaneSelect },
   { key = 'o', mods = 'LEADER', action = act.RotatePanes 'Clockwise' },
-  { key = 'c', mods = 'LEADER', action = act.SpawnCommandInNewTab { args = { 'nvim', wezterm.home_dir .. '/.wezterm.lua' }}},
+  { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection 'Left' },
+  { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection 'Down' },
+  { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection 'Up' },
+  { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection 'Right' },
 }
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.95
+config.scrollback_lines = 10000
 
 return config
--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Map TableGen'erated files to the MLIR file type
 vim.filetype.add({
   extension = {
     mlir = 'mlir',
