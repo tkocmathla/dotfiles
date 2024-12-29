b6fbfe7897bd0c71ef2214e6833a19ebe247c460
tkocmathla <matt.t.grimm@gmail.com>
2024-12-29 12:17:52 -0700

    QOL for formatting

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -3,7 +3,7 @@
 -- Add any additional autocmds here
 local locals = require("config.locals")
 
--- Map TableGen'erated files to the MLIR file type
+-- MLIR: Map TableGen'erated files to the MLIR file type
 vim.filetype.add({
   extension = {
     mlir = 'mlir',
@@ -12,6 +12,30 @@ vim.filetype.add({
     ['.*[.]h[.]inc'] = 'cpp',
     ['.*[.]cpp[.]inc'] = 'cpp',
   },
+})
+
+-- C++: Format code on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.cpp',
+    callback = function()
+        vim.lsp.buf.format()
+    end,
+})
+
+-- Go: Don't show visible tabs
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*.go',
+    callback = function()
+        vim.opt.listchars:append({ tab = "  " })
+    end,
+})
+
+-- Go: Format code and imports on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    callback = function()
+       require('go.format').gofmt()
+    end,
 })
 
 locals.autocmds()
