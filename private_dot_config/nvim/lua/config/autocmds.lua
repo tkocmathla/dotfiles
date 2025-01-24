ec7e18e1943d4539e45b0cd71ce86070f6c55b12
tkocmathla <matt.t.grimm@gmail.com>
2025-01-24 12:18:33 -0700

    [nvim] format python code on save

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -3,11 +3,15 @@
 -- Add any additional autocmds here
 local locals = require("config.locals")
 
--- MLIR: Map TableGen'erated files to the MLIR file type
+-- MLIR: Set filetype based on extension
 vim.filetype.add({
   extension = {
     mlir = 'mlir',
   },
+})
+
+-- MLIR: Map TableGen'erated C++ files to the cpp file type
+vim.filetype.add({
   pattern = {
     ['.*[.]h[.]inc'] = 'cpp',
     ['.*[.]cpp[.]inc'] = 'cpp',
@@ -17,6 +21,14 @@ vim.filetype.add({
 -- C++: Format code on save
 vim.api.nvim_create_autocmd('BufWritePre', {
     pattern = '*.cpp',
+    callback = function()
+        vim.lsp.buf.format()
+    end,
+})
+
+-- Python: Format code on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.py',
     callback = function()
         vim.lsp.buf.format()
     end,
