300e40604d8152ac43939de792fbf2fca281458f
tkocmathla <matt.t.grimm@gmail.com>
2025-01-28 06:54:10 -0700

    [nvim] remove dead lsp-status config

--- private_dot_config/nvim/lua/plugins/lsp-status.lua
+++ private_dot_config/nvim/lua/plugins/lsp-status.lua
@@ -3,14 +3,5 @@ return {
     config = function()
         local lsp_status = require('lsp-status')
         lsp_status.register_progress()
-
-        require('lspconfig').clangd.setup({
-            handlers = lsp_status.extensions.clangd.setup(),
-            --init_options = {
-            --clangdFileStatus = true
-            --},
-            on_attach = lsp_status.on_attach,
-            capabilities = lsp_status.capabilities
-        })
     end
 }
