0e8226b9e4b347a0ecbca03b4e1a61e2d2fc3dd1
tkocmathla <matt.t.grimm@gmail.com>
2025-08-10 07:47:39 +0100

    don't format py on save, add vertical wrap marker

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -26,11 +26,19 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     end,
 })
 
--- Python: Format code on save
-vim.api.nvim_create_autocmd('BufWritePre', {
+-- C++: Show vertical bar for line wrap
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*.cpp',
+    callback = function()
+        vim.opt.colorcolumn = "100"
+    end,
+})
+
+-- Python: Show vertical bar for line wrap
+vim.api.nvim_create_autocmd('BufEnter', {
     pattern = '*.py',
     callback = function()
-        vim.lsp.buf.format()
+        vim.opt.colorcolumn = "90"
     end,
 })
 
